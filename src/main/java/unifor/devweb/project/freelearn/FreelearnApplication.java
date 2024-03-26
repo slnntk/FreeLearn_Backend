8deb8b1d5f419e8eb1bef19079576cfcf2294c6b
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2024-03-26 13:28:45 -0300

    first commit

---
+++ src/main/java/unifor/devweb/project/freelearn/FreelearnApplication.java
@@ -0,0 +1,13 @@
+package unifor.devweb.project.freelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unifor/devweb/project/freelearn/FreelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package unifor.devweb.project.freelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
