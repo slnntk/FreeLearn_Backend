cef5483b6a341073d3071570f6cbf184cafaac86
Cássio Castro <101597036+slnntk@users.noreply.github.com>
2024-05-20 14:47:29 -0300

    🔒 Update: Adding authentication check for creating users with administrator permissions
     Now, to create an administrator user, the user must be an authenticated administrator. Methods were created in the SecurityFilter class to verify if a user is an administrator (`isAdmin`) and if they are authenticated (`isAuthenticated`). These checks have been implemented in the AuthenticationController class.

--- src/main/java/unifor/devweb/project/freelearn/controller/security/AuthenticationController.java
+++ src/main/java/unifor/devweb/project/freelearn/controller/security/AuthenticationController.java
@@ -3,18 +3,26 @@ package unifor.devweb.project.freelearn.controller.security;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.core.service.SecurityService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import unifor.devweb.project.freelearn.domain.entities.user.User;
+import unifor.devweb.project.freelearn.domain.entities.user.UserRole;
 import unifor.devweb.project.freelearn.dto.security.AuthenticationDTO;
 import unifor.devweb.project.freelearn.dto.security.LoginResponseDTO;
 import unifor.devweb.project.freelearn.dto.security.RegisterDTO;
+import unifor.devweb.project.freelearn.exception.AccessDeniedException;
+import unifor.devweb.project.freelearn.exception.BadRequestException;
+import unifor.devweb.project.freelearn.infra.security.SecurityFilter;
 import unifor.devweb.project.freelearn.infra.security.TokenService;
 import unifor.devweb.project.freelearn.repository.UserRepository;
 
@@ -27,29 +35,41 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final TokenService tokenService;
+    private final SecurityFilter securityFilter;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+    public ResponseEntity performLogin(@RequestBody @Valid AuthenticationDTO authenticationData) {
         String token = null;
-        try{
-            var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
-            var auth = this.authenticationManager.authenticate(usernamePassword);
-            token = tokenService.generateToken((User)auth.getPrincipal());
-        } catch (Exception e){
+        try {
+            var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(authenticationData.email(), authenticationData.password());
+            var authentication = this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+            token = tokenService.generateToken((User) authentication.getPrincipal());
+        } catch (Exception e) {
             log.error("error");
         }
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
-        if(this.userRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+    public ResponseEntity performRegistration(@RequestBody @Valid RegisterDTO registrationData) {
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.email(), data.name(), encryptedPassword, data.role());
+        if (this.userRepository.findByEmail(registrationData.email()) != null) return ResponseEntity.badRequest().build();
+
+        RegisterDTO processedRegistrationData = processRegistrationData(registrationData);
+        String encryptedPassword = new BCryptPasswordEncoder().encode(processedRegistrationData.password());
+        User newUser = new User(processedRegistrationData.email(), processedRegistrationData.name(), encryptedPassword, processedRegistrationData.role());
 
         this.userRepository.save(newUser);
 
         return ResponseEntity.ok().build();
+    }
+
+    private RegisterDTO processRegistrationData(RegisterDTO registrationData) {
+
+        if (!securityFilter.isAdmin() && UserRole.ADMIN.equals(registrationData.role())) {
+            throw new AccessDeniedException("You do not have permission to create this user");
+        }
+
+        return registrationData;
     }
 }
--- src/main/java/unifor/devweb/project/freelearn/infra/security/SecurityFilter.java
+++ src/main/java/unifor/devweb/project/freelearn/infra/security/SecurityFilter.java
@@ -5,7 +5,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -15,16 +18,20 @@ import unifor.devweb.project.freelearn.repository.UserRepository;
 import java.io.IOException;
 
 @Component
+@Log4j2
 @RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
     private final UserRepository userRepository;
 
+    @Value("${user.unauthenticated}")
+    private String UNAUTHENTICATED_USER;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if(token != null){
+        if (token != null) {
             var login = tokenService.validateToken(token);
             UserDetails user = userRepository.findByEmail(login);
 
@@ -34,9 +41,20 @@ public class SecurityFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
+    private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
+        if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
+
+    public boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated() && !UNAUTHENTICATED_USER.equals(authentication.getName());
+    }
+
+    public boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+    }
+
 }
