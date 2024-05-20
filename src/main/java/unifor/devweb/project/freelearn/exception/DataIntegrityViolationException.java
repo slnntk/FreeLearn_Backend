63bb3efa939b7697f9ddbc487a08c536f08b1366
Cássio Castro <101597036+slnntk@users.noreply.github.com>
2024-05-20 14:49:45 -0300

    🚀 Update: Addition of new exceptions and implementation in the exception handler
    New exceptions have been added and implemented in the exception handler.

---
+++ src/main/java/unifor/devweb/project/freelearn/exception/DataIntegrityViolationException.java
@@ -0,0 +1,11 @@
+package unifor.devweb.project.freelearn.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DataIntegrityViolationException extends RuntimeException {
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+}
